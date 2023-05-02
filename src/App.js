45faddfe48be6b2fa5199ba2a9baede67065e5d1
Chyniece Matthews <cfmatthews2@gmail.com>
2023-05-02 11:04:51 -0500

    added template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
-import Explore from './Explore';
 import axios from "axios";
-//import { useState } from 'react';
+import { useState } from 'react';
 //import { useEffect } from 'react';
 
 function App() {
-  //const [myCity, setMyCity] = useState("");
+  const [myCity, setMyCity] = useState('');
 
   const fetchData = async () => {
-    const response = await axios.get("https://us1.locationiq.com/v1/search?key=pk.e65687e540287de5bf7920f2c5a4d514&q=Memphis%2CTennessee&format=json");
-    console.log("response", response)
-   // return setMyCity(response.data);
+    const response = await axios.get(`https://us1.locationiq.com/v1/search?key=pk.e65687e540287de5bf7920f2c5a4d514&q=${myCity}&format=json`);
+    console.log("response", response.data)
+   return response.data;
   }
 
-//  // useEffect(() => {
-//     fetchData();
-  // }, [])
+ const handleCity = (e) =>{
+  setMyCity(e.target.value);
+ };
   return (
     <div className="App">
-      <Explore fetchData={fetchData} />
+     <label>
+      Enter City Name:
+        <input type="text" value={myCity} onChange={handleCity} id="cityInput" placeholder="City Name" />
+        <button onClick={fetchData}>Explore!</button>
+    </label>
     </div>
   );
 }
