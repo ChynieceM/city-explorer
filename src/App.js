7fee9eb46becce0fdf709c0e4697b56251d0d4fa
Chyniece Matthews <cfmatthews2@gmail.com>
2023-05-02 14:21:14 -0500

    added try catch

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import { useState } from 'react';
 
 function App() {
   const [myCity, setMyCity] = useState('');
-
   const fetchData = async () => {
-    const response = await axios.get(`https://us1.locationiq.com/v1/search?key=pk.e65687e540287de5bf7920f2c5a4d514&q=${myCity}&format=json`);
-   console.log("response", response.data)
-   return response.data;
+    try {
+      const response = await axios.get(`https://us1.locationiq.com/v1/search?key=pk.e65687e540287de5bf7920f2c5a4d514&q=${myCity}&format=json`);
+      //console.log("response", response.data)
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
   }
-
- const handleCity = (e) =>{
-  setMyCity(e.target.value);
- };
+  const handleCity = (e) => {
+    setMyCity(e.target.value);
+  };
   return (
     <div className="App">
-     <label>
-      Enter City Name: 
+      <label>
+        Enter City Name:
         <input type="text" value={myCity} onChange={handleCity} id="cityInput" placeholder="City Name" />
         <button onClick={fetchData}>Explore!</button>
-    </label>
+      </label>
     </div>
   );
 }
