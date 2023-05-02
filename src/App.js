94b5a0db55cdea93673ff1ca8683139311cbdb39
Chyniece Matthews <cfmatthews2@gmail.com>
2023-05-02 14:11:54 -0500

    console.log

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const fetchData = async () => {
     const response = await axios.get(`https://us1.locationiq.com/v1/search?key=pk.e65687e540287de5bf7920f2c5a4d514&q=${myCity}&format=json`);
-   // console.log("response", response.data)
+   console.log("response", response.data)
    return response.data;
   }
 
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="App">
      <label>
-      Enter City Name:
+      Enter City Name: 
         <input type="text" value={myCity} onChange={handleCity} id="cityInput" placeholder="City Name" />
         <button onClick={fetchData}>Explore!</button>
     </label>
