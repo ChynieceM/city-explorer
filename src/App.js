83bc234b3c53d60a7dada8f0c85ab05a64943096
Chyniece Matthews <cfmatthews2@gmail.com>
2023-05-18 14:15:40 -0500

    lab 9

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import React, { useEffect } from 'react';
 import axios from "axios";
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button, Form } from 'react-bootstrap';
+import { Button} from 'react-bootstrap';
 import Weather from './Weather';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerIconShadow from 'leaflet/dist/images/marker-shadow.png';
+import Map from './Map';
 import './rain.css';
 import Movie from './Movie';
-//rain animation requirements 
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon,
-  iconUrl: markerIcon,
-  shadowUrl: markerIconShadow,
-});
+import Location from './Location';
 
 
 function App() {
@@ -27,16 +16,20 @@ function App() {
   const [location, setLocation] = useState('');
   const [responseData, setResponseData] = useState([]);
   const [input, setInput] = useState('');
+
   const [myCity, setMyCity] = useState('');
   const [displayCity, setdisplayCity] = useState('');
   const [displayMap, setdisplayMap] = useState('none');
+
   const [movies, setMovies] = useState([]);
+  
   const [isRaining, setIsRaining] = useState(false);
-//for rain animation
+ 
+  //for rain animation
   const generateRaindrops = () => {
     const numberOfDrops = 100;
     const drops = [];
-  
+
     for (let i = 0; i < numberOfDrops; i++) {
       const delay = Math.random() * 1;
       const speed = Math.random() * 0.7 + 0.7;
@@ -53,21 +46,22 @@ function App() {
         ></div>
       );
     }
-  
+
     return drops;
   };
-const fetchMovieData = async () => {
-  try {
-    let response = await axios.get(`https://city-explorer-api-sdwd.onrender.com/movies?searchQuery=${input}`)
-    setMovies(response.data)
-  } catch (error) {
-    console.log(error)
+
+  const fetchMovieData = async () => {
+    try {
+      let response = await axios.get(`https://city-explorer-api-sdwd.onrender.com/movies?searchQuery=${input}`)
+      setMovies(response.data)
+    } catch (error) {
+      console.log(error)
+    }
   }
-}
   //fetches data from our weather API based on the location
   const fetchLocationData = async () => {
     try {
-      
+
       let response = await axios.get(`https://city-explorer-api-sdwd.onrender.com/weather/?searchQuery=${input}`)
       //Updating responseData with the first result from the API response
       setResponseData(response.data)
@@ -86,7 +80,7 @@ const fetchMovieData = async () => {
   useEffect(() => {
     if (location !== '') {
       fetchLocationData(fetchMovieData());
-      
+
     }
   }, [location]);
 
@@ -128,52 +122,17 @@ const fetchMovieData = async () => {
   };
   return (
     <div style={{ height: '100vh', padding: '20px', margin: '10px 10px 10px 10px', border: '2px solid #990099' }}>
-      <Form style={{ display: "flex", justifyContent: "center", alignItems: "center", }}>
-        <fieldset>
-          <Form.Group className="mb-3">
-            <Form.Label style={{ fontWeight: 'bold', fontSize: '50px' }} >Google Maps Misfits</Form.Label>
-            <Form.Control type="text" value={myCity} onChange={handleCity} id="cityInput" placeholder="City Name" />
-          </Form.Group>
-          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", }}>
-            <Button style={{ background: ' #35495e', cursor: 'pointer', padding: '0px 40px 5px 40px', margin: '3px 30 30 30px' }} onClick={(e) => {
-              e.preventDefault()
-              setLocation(input)
-              fetchData()
-              //setLocation function sets the location state variable to the current value of the input state variable
-            }}>Explore</Button>
-          </div>
-        </fieldset>
-      </Form>
+      <Location setLocation={setLocation} myCity={myCity} input={input} fetchData={fetchData} handleCity={handleCity} displayCity={displayCity}/>
       <div >
-        <div>Location: {displayCity.display_name}</div>
-        <div>Latitude: {displayCity.lat}</div>
-        <div>Longitude: {displayCity.lon}</div>
         <div>{threeDayForecast}</div>
         <Button onClick={() => setIsRaining(!isRaining)} style={{ background: ' #35495e', cursor: 'pointer', padding: '0px 40px 5px 40px', margin: '30px 0px 0px 30px' }}>Let it rain</Button>
-        {isRaining && <div className="rain">{generateRaindrops()}</div>}
+        {isRaining && <div className="rain">{generateRaindrops()}
+        </div>
+        }
 
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", }} >
-          {/* renders the Weather component w/ props based on responseData state variable */}
-          <Movie movies={movies}/>
-          <Card style={{ display: displayMap, marginBottom: '10px', marginTop: '0px', height: '40%', width: '40%' }}>
-            {displayCity.lat && displayCity.lon ? (
-              <MapContainer
-                center={[displayCity.lat, displayCity.lon]}
-                zoom={12}
-                style={{ display: displayMap, height: "500px", width: "600px" }} scrollWheelZoom={false}
-              >
-                <TileLayer
-                  url="https://{s}-tiles.locationiq.com/v3/streets/r/{z}/{x}/{y}.png?key=pk.e65687e540287de5bf7920f2c5a4d514"
-                  attribution='&copy; <a href="https://www.locationiq.com/">LocationIQ</a> contributors'
-                />
-                <Marker
-                  position={[displayCity.lat, displayCity.lon]}>
-                  <Popup> {[displayCity.display_name]}</Popup>
-                </Marker>
-              </MapContainer>
-            ) : null}
-
-          </Card>
+          <Movie movies={movies} />
+          <Map displayMap={displayMap} displayCity={displayCity}/>
         </div>
       </div>
     </div >
--- src/Movie.js
+++ src/Movie.js
@@ -3,31 +3,31 @@ import React from 'react';
 
 
 function Movie(props) {
-    let movie = props.movies;
-    return (
-
-        <Carousel>
-            {movie.map(element => {
+  let movie = props.movies;
   return (
-    <Carousel.Item key={element.id}>
-      <img
-        className="d-block w-100"
-        src={element.poster_path}
-        alt={element.overview}
-      />
-      <Carousel.Caption>
-        <h3>{element.title}</h3>
-        <p>{element.popularity}</p>
-        <p>{element.release_date}</p>
-        <p>{element.vote_average}</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  );
-})}
-            
-        </Carousel >
 
-    )
+    <Carousel>
+      {movie.map(element => {
+        return (
+          <Carousel.Item key={element.id}>
+            <img
+              className="d-block w-100"
+              src={element.poster_path}
+              alt={element.overview}
+            />
+            <Carousel.Caption>
+              <h3>{element.title}</h3>
+              <p>{element.popularity}</p>
+              <p>{element.release_date}</p>
+              <p>{element.vote_average}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        );
+      })}
+
+    </Carousel >
+
+  )
 
 
 }
